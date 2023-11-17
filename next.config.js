b8182e56baef98a62c2d70355014f51399b18c1e
dd3dd <jacky81810@gmail.com>
2023-11-17 20:50:21 +0800

    sdfs

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
-const dns = require("dns");
 
-dns.setDefaultResultOrder("ipv4first")
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         domains: ["lh3.googleusercontent.com"],
     },
-    env: {
-        NEXT_PUBLIC_API_URL: "https://commentboard.vercel.app",
-        NEXTAUTH_URL: "https://commentboard.vercel.app"
-    }
-
 };
 
 module.exports = nextConfig;
