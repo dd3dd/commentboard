8c50fbe4ef6df9d58f8e1d96cb0074917e5febe7
dd3dd <jacky81810@gmail.com>
2023-11-21 00:47:23 +0800

    aa

--- app/page.jsx
+++ app/page.jsx
@@ -1,30 +1,32 @@
 import Post from "@/components/Post";
 import { IoMdAdd } from "react-icons/io";
 import Link from "next/link";
-const show = async () => {
-  try {
-    const res = await fetch(process.env.NEXT_PUBLIC_API_URL + '/api/post', {
-      cache: "no-store",
-    });
+// const show = async () => {
+//   try {
+//     const res = await fetch(process.env.NEXT_PUBLIC_API_URL + '/api/post', {
+//       cache: "no-store",
+//     });
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch topics");
-    }
-    return res.json();
-  } catch (error) {
-    console.log(error);
-  }
-};
+//     if (!res.ok) {
+//       throw new Error("Failed to fetch topics");
+//     }
+//     return res.json();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 export default async function Page() {
-  const { post } = await show();
+  // const { post } = await show();
   return (
-    <div className="relative flex flex-col">
-      <Link className='absolute top-4 right-0' href={`/addpost`}>
-        <IoMdAdd size={40} />
-      </Link>
-      {post.map(e =>
-        <Post key={e._id} id={e._id} email={e.email} name={e.name} img={e.img} title={e.title} content={e.content} />
-      )}
-    </div>
+    <>
+      {/* <div className="relative flex flex-col">
+        <Link className='absolute top-4 right-0' href={`/addpost`}>
+          <IoMdAdd size={40} />
+        </Link>
+        {post.map(e =>
+          <Post key={e._id} id={e._id} email={e.email} name={e.name} img={e.img} title={e.title} content={e.content} />
+        )}
+      </div> */}
+    </>
   )
 }
